66e8a30570b88c1503f26177bdef631006bea232
Shivam Singh <shivamsi687@gmail.com>
2022-12-03 16:41:54 +0530

    update sitemap 2

--- lib/SitemapGeneratorV2.ts
+++ lib/SitemapGeneratorV2.ts
@@ -5,25 +5,46 @@ import { AllBanksList } from '../bank_data/allBankList';
 import path from 'path';
 const bankList = AllBanksList;
 
-async function init(){
-   
+async function init() {
+
     let currentFileIndex = 0;
     let currentFile = null;
 
-    await sitemapByBankCode('ABHY').then(val => {
-        const sitmapArr = getSitmap(val);
-        writeBankData(sitmapWrapper(sitmapArr), 'abhy.xml');
-    })
+    // const createdSitemapLinks = await createChunksSitemap();
+    // console.log("CHUNKS:: ", createdSitemapLinks)
+    const list = Object.keys(AllBanksList).map(val => `${val}.xml`)
+    const sitmapString = sitmapIndexWrapper(list);
+    writeBankData(sitmapString, path.join('public', 'sitemap.xml'))
+
 }
 
-async function bankDataLoader(currentIndex: number, codelist: string[]){
-    if(currentIndex >= codelist.length) return;
+async function createChunksSitemap() {
+    const codes = Object.keys(AllBanksList);
+    const createdLinks: string[] = [];
+    console.log("Chunks creating: ")
+    codes.forEach(async (code) => {
+        await sitemapByBankCode(code).then(val => {
+            const sitmapArr = getSitmap(val);
+            const fileName = `${code}.xml`
+            const p = path.join('public', 'sitemaps', fileName);
+            writeBankData(sitmapWrapper(sitmapArr), p);
+            createdLinks.push(fileName);
+        })
+    })
+    console.log("Chunks created");
+    
+
+    return createdLinks;
+}
+
+async function bankDataLoader(currentIndex: number, codelist: string[]) {
+    if (currentIndex >= codelist.length) return;
     try {
         const code = codelist[currentIndex];
-        if(!code)return;
+        if (!code) return;
         // console.log("Code",code, codelist)
         const bankData = BankMap[code as keyof typeof BankMap];
-        const banks: {[key:string]:BankModel} = await (await bankData()).default;
+        const banks: { [key: string]: BankModel } = await (await bankData()).default;
         const prevData = await bankDataLoader(currentIndex++, codelist);
         const bankList = [...Object.values(banks)];
         bankList.push(...prevData || [])
@@ -33,17 +54,17 @@ async function bankDataLoader(currentIndex: number, codelist: string[]){
     }
 }
 
-async function writeBankData(data: string, filename?:string){
+
+
+async function writeBankData(data: string, path: string) {
     try {
-        console.log("Start writing")
-        const p = path.join('public', filename||'sitemap.xml')
-        fs.writeFileSync(p, data);
+        fs.writeFileSync(path, data);
     } catch (error) {
         console.error(error)
     }
 }
 
-function getSitmap(banks: BankModel[]){
+function getSitmap(banks: BankModel[]) {
     const sitemapArray = banks.map(bank => {
         const url = `${bank.BANK}/${bank.STATE}/${bank.DISTRICT}/${bank.BRANCH}`.toLowerCase().replaceAll('&', '&amp;').replaceAll('-', '').trim()
         const str = `<url><loc>https://www.findifscode.in/${encodeURI(url)}</loc><lastmod>${new Date().toISOString()}</lastmod><changefreq>daily</changefreq><priority>1.0</priority></url>`
@@ -52,23 +73,39 @@ function getSitmap(banks: BankModel[]){
     return sitemapArray;
 }
 
-async function sitemapByBankCode(code: string){
+async function sitemapByBankCode(code: string) {
     const bankData = BankMap[code as keyof typeof BankMap];
-    const data: {[key:string]:BankModel} = await (await bankData()).default;
+    const data: { [key: string]: BankModel } = await (await bankData()).default;
     const values = Object.values(data);
     return values;
 }
 
- function sitmapWrapper(data: string[]|string){
+function sitmapWrapper(data: string[] | string) {
     const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
-    ${typeof data === 'object' && Array.isArray(data) ?  data.join('\n') : data}
+    ${typeof data === 'object' && Array.isArray(data) ? data.join('\n') : data}
     </urlset>`;
     return sitemap;
 }
 
+function sitmapIndexWrapper(sitemapLinks: string[]) {
+    const sitemap = `
+    <?xml version="1.0" encoding="UTF-8"?>
+    <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ${sitemapLinks.map(link => {
+        return `
+        <sitemap>
+            <loc>https://www.ifscode.in/sitemaps/${link}</loc>
+        </sitemap>
+        `
+    }).join('\n')}
+    </sitemapindex>
+    `
+    return sitemap;
+}
 
- const SitemapGeneratorV2 = {
+
+const SitemapGeneratorV2 = {
     init
 }
 
